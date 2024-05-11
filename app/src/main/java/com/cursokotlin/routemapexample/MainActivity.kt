9822dc0aaf417b3e6249d2e322cf6001d63ae7cb
Jose Luis Aguirre Ramirez <jluis.aguirre@udea.edu.co>
2024-05-11 06:56:49 -0500

    expo

---
+++ app/src/main/java/com/cursokotlin/routemapexample/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.cursokotlin.routemapexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//implementar callbac
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+
+
+
+
+
+
+
+
+}
